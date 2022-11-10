9ec6eac3efd3028fcdab843bd3a48e16625b944e
rodo08 <rodrigo.rosalesmoya@gmail.com>
2022-11-10 18:34:06 +0100

    Add user controllers and update userRouter exports

--- src/server/routers/usersRouter.ts
+++ src/server/routers/usersRouter.ts
@@ -1,5 +1,7 @@
 import express from "express";
+import loginUser from "../../controllers/userController";
 
+// eslint-disable-next-line new-cap
 const usersRouter = express.Router();
 
 usersRouter.post("/login", loginUser);
